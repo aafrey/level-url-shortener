fff001d422b0066cb997fb0b9985dd2d86f421e1
System <none>
2017-07-16 19:29:08 +0000

    Checkpoint

--- db-connect.js
+++ db-connect.js
@@ -2,3 +2,13 @@ const mongo = require('mongodb').MongoClient
 
 const url = 'mongodb://' + process.env.DBUSER + ':' + process.env.DBPW + '@ds147872.mlab.com:47872/glitch' 
 
+const connect = (x) => {
+  mongo.connect(url, (err, db) => {
+    if (err) throw err
+    
+    
+  })
+}
+
+module.exports = connect
+
--- server.js
+++ server.js
@@ -1,12 +1,30 @@
+const connect = require('./db-connect')
 const express = require('express');
 const app = express();
 
 app.use(express.static('public'));
 
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
 });
+
+app.get('/:url', (req, res) => {
+  // parse params
+  connect(url)
+  //responde with JSON 
+})
 
 const listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+
+/*
+Grab to be shortened URL from params
+
+parse to ensure its valid
+
+addd a new entry to db collection that matches the shortened URL with the original
+
+return it to client
+*/
