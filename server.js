197aa2c5b901956bcfaec84fdfd74ff83f751053
System <none>
2017-07-17 01:17:25 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ const listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
 
-
+console.log(process.env.DBUSER)
+console.log(process.env.DBPw)
 /*
 Grab to be shortened URL from params
 
