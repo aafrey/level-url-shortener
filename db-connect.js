f1f7af2c8ed024f98292a53dfc92103cbb2b4787
System <none>
2017-07-17 00:44:25 +0000

    Checkpoint

--- db-connect.js
+++ db-connect.js
@@ -1,20 +1,23 @@
 const mongo = require('mongodb').MongoClient
 
-const url = 'mongodb://' + process.env.DBUSER + ':' + process.env.DBPW + '@ds147872.mlab.com:47872/glitch' 
+const url = 'mongodb://' + process.ENV.DBUSER + ':' + process.ENV.DBPW + '@ds147872.mlab.com:47872/glitch' 
 
 const connect = (urlId, actualUrl) => {
   mongo.connect(url, (err, db) => {
     if (err) throw err
     
     var collection = db.collection('urls')
-    var id = collection.find({url: 'numIds'})
-    console.log(id)
+    
+    collection.find({url: 'numIds'}).toArray((err, docs) => {
+      console.log(docs)
+      db.close()
+    })
     //collection.insert('')
     
-  }).toArray((err, docs) => {
-    db.close()
   })
 }
 
-module.exports = connect
+connect()
+
+//module.exports = connect
 
