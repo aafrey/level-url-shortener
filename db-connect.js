8fb79125952fff5f527fd1725532a4009c90a6fe
System <none>
2017-07-17 22:33:43 +0000

    Checkpoint

--- db-connect.js
+++ db-connect.js
@@ -7,18 +7,20 @@ const connect = (url, res) => {
     if (err) throw err
 
     const collection = db.collection('urls')
-    // TODO fix this query, it's always returns null
-    collection.find({_id: 'url info'}, (err, docs) => {
-      if (err) throw err
 
-      if (docs === null) {
-        collection.insertMany([{_Id: 'url info', numIds: 0}, {urls: {0: url}}], (err, r) => {
+    collection.find({urlId: 'url info'}).toArray((err, docs) => {
+      if (err) throw err
+      console.log(docs)
+      if (docs.length === 0) {
+        
+        collection.insertMany([{_Id: 'url info', numIds: 0}, {0: url}], (err) => {
           if (err) throw err
 
           const urlsToSend = {normal: url, shortened: 'https://nickel-value.glitch.me/0'}
 
           db.close()
           res.end(JSON.stringify(urlsToSend))
+          
         })
       } else {
         res.end('todo')
