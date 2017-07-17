9ac437064fc61efd8c269b2689f146c6855cddb4
System <none>
2017-07-17 22:11:44 +0000

    Checkpoint

--- db-connect.js
+++ db-connect.js
@@ -4,22 +4,16 @@ const mongoUri = process.env.MONGO_URI
 
 const connect = (url, res) => {
   mongo.connect(mongoUri, (err, db) => {
-    if (err) {
-      throw err
-    }
+    if (err) throw err
 
     const collection = db.collection('urls')
     // TODO fix this query, it's always returns null
-    collection.findOne({urlId: 'numIds'}, (err, docs) => {
-      if (err) {
-        throw err
-      }
+    collection.find({urlId: {$exists: true}}, (err, docs) => {
+      if (err) throw err
 
       if (docs === null) {
         collection.insertMany([{urlId: {numIds: 0}}, {urls: {0: url}}], (err, r) => {
-          if (err) {
-            throw err
-          }
+          if (err) throw err
 
           const urlsToSend = {normal: url, shortened: 'https://nickel-value.glitch.me/0'}
 
