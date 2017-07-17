31c4f2d74926e47d0682f857d65cc1f10e8aba90
System <none>
2017-07-17 22:44:43 +0000

    Checkpoint

--- db-connect.js
+++ db-connect.js
@@ -8,23 +8,28 @@ const connect = (url, res) => {
 
     const collection = db.collection('urls')
 
-    collection.find({urlId: 'url info'}).toArray((err, docs) => {
+    collection.find({_id: 'url info'}).toArray((err, docs) => {
       if (err) throw err
-      console.log(docs)
+      console.log(docs[0].numIds)
       if (docs.length === 0) {
         
-        collection.insertMany([{_Id: 'url info', numIds: 0}, {0: url}], (err) => {
+        collection.insertMany([{_id: 'url info', numIds: 0}, {0: url}], (err) => {
           if (err) throw err
 
-          const urlsToSend = {normal: url, shortened: 'https://nickel-value.glitch.me/0'}
+          var urlsToSend = {normal: url, shortened: 'https://nickel-value.glitch.me/0'}
 
           db.close()
           res.end(JSON.stringify(urlsToSend))
           
         })
       } else {
-        res.end('todo')
-        db.close()
+        collection.update({_id: 'url info'}, {$inc: {numIds: 1}}).then((err) => {
+          if (err) throw err
+          
+          var urlToSend = {normal: url, shortened: ''}
+          res.end()
+          db.close()  
+        })
       }
     })
   })
