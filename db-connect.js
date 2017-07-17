53391cf4c8e31c460f79844ed45e347934c099be
System <none>
2017-07-17 03:08:37 +0000

    Checkpoint

--- db-connect.js
+++ db-connect.js
@@ -3,27 +3,22 @@ const mongo = require('mongodb').MongoClient
 const mongoUri = process.env.MONGO_URI 
 
 const connect = (url) => {
-  var shortenedUrl
+  var shortUrl
   mongo.connect(mongoUri, (err, db) => {
     if (err) throw err
-    
+    var foo
     var collection = db.collection('urls')
-    var short
-    collection.findOne({url: 'numIds'}, (err, docs) => {
+    
+    var currentUrlId = collection.findOne({url: 'numIds'}, (err, docs) => {
       if (err) throw err
-      var short
-      if (docs === null) {
-        collection.insertMany([{url: {numIds: 0}}, {url: {0: url}}], () => {
-          short = 'https://nickel-value.glitch.me/0'
-        })
-      } else {
-        short = 'https://nickel-value.glitch.me/' + docs[0].toString()
-      }
+      foodocs
     })
+    
+    //console.log(currentUrlId)
+    
     db.close()
-    console.log(short)
   })
-  //console.log(shortUrl)
+  return shortUrl
 }
 
 module.exports = connect
