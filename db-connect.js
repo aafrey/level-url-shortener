5a18d11046bc71bf3aabcefffd5dbac861da7119
System <none>
2017-07-17 22:55:43 +0000

    Checkpoint

--- db-connect.js
+++ db-connect.js
@@ -4,31 +4,41 @@ const mongoUri = process.env.MONGO_URI
 
 const connect = (url, res) => {
   mongo.connect(mongoUri, (err, db) => {
-    if (err) throw err
+    if (err) {
+      throw err
+    }
 
     const collection = db.collection('urls')
 
     collection.find({_id: 'url info'}).toArray((err, docs) => {
-      if (err) throw err
-      console.log(docs[0].numIds)
-      if (docs.length === 0) {
-        
-        collection.insertMany([{_id: 'url info', numIds: 0}, {0: url}], (err) => {
-          if (err) throw err
+      if (err) {
+        throw err
+      }
 
-          var urlsToSend = {normal: url, shortened: 'https://nickel-value.glitch.me/0'}
+      if (docs.length === 0) {
+        collection.insertMany([{_id: 'url info', numIds: 0}, {0: url}], err => {
+          if (err) {
+            throw err
+          }
+
+          const urlsToSend = {normal: url, shortened: 'https://nickel-value.glitch.me/0'}
 
           db.close()
           res.end(JSON.stringify(urlsToSend))
-          
         })
       } else {
-        collection.update({_id: 'url info'}, {$inc: {numIds: 1}}).then((err) => {
-          if (err) throw err
+        collection.update({_id: 'url info'}, {$inc: {numIds: 1}}, (err) => { 
           
-          var urlToSend = {normal: url, shortened: ''}
-          res.end()
-          db.close()  
+          if (err) {
+            throw err
+          }
+          collection.insert({})
+
+          const urlId = docs[0].numIds + 1
+          const urlToSend = {normal: url, shortened: 'https://nickel-value.glitch.me/' + urlId}
+
+          db.close()
+          res.end(JSON.stringify(urlToSend))
         })
       }
     })
