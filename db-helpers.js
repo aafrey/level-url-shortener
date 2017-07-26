12d42d23759dd6a0c5021a8f1b4f4939b0b169df
System <none>
2017-07-26 00:04:23 +0000

    Checkpoint

--- db-helpers.js
+++ db-helpers.js
@@ -2,33 +2,21 @@ const db = require('./db')
 
 const shortUrl = process.env.SHORT_URL
 
+const getUrl = (urlId, res) => {
+  res.status(200).end(db.get(urlId))
+}
+
 const connect = (url, res) => {
    var _id
    if (db.get('_id') === undefined) { _id = db.put('_id', '0') }
    else { _id = db.get('_id') }
-   const urlToSend = {normal: url, shortUrl: shortUrl + _id }
+   const urlsToSend = {normal: url, shortUrl: shortUrl + _id }
    
    (parseInt(_id), _id++)
    db.put('_id', parseInt(_id))
      
    res.status(200).end(JSON.stringify(urlsToSend))
-
-         } else {
-            collection.update(
-               {_id: 'url info'}, {$inc: {numIds: 1}}
-            ).then(() => {
-               const urlId = docs[0].numIds + 1
-               collection.insert({_id: urlId, url})
-               return urlId
-            }).then(urlId => {
-               const urlToSend = {normal: url, shortened: shortUrl + urlId}
-               res.status(200).end(JSON.stringify(urlToSend))
-               db.close()
-            }).catch(err => console.log(err))
-         }
-      })
-   })
-}
+} 
 
 module.exports = {
    connect,
