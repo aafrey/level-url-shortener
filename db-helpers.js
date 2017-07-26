49fff740fafa13051e5c7423447428c7b6a2c448
System <none>
2017-07-26 01:43:23 +0000

    Checkpoint

--- db-helpers.js
+++ db-helpers.js
@@ -2,22 +2,20 @@ const db = require('./db')
 
 const shortUrl = process.env.SHORT_URL
 
-const getNextId = () => {
-   return new Promise((resolve, reject) => {
-      db.get('_id', (err, val) => {
-         if (err) {
-            reject(err)
-         }
-         if (val === undefined) {
-            db.put('_id', '0')
-            resolve('0')
-         } else {
-            console.log(val)
-            resolve(val)
-         }
-      })
+const getNextId = new Promise((resolve, reject) => {
+   db.get('_id', (err, val) => {
+      if (err) {
+         reject(err)
+      }
+      if (val === undefined) {
+         db.put('_id', '0')
+         resolve('0')
+      } else {
+         console.log(val)
+         resolve(val)
+      }
    })
-}
+})
 
 const getUrl = id => {
    return new Promise((resolve, reject) => {
@@ -26,20 +24,17 @@ const getUrl = id => {
 }
 
 const connect = (url, res) => {
-   getNextId()
-   .then(id => {
+   getNextId.then(id => {
       res.status(200).send(JSON.stringify({normal: url, shortUrl: shortUrl + id}))
       db.put(id, url)
       return id
    })
    .then(id => {
-      id = parseInt(id)
+      id = parseInt(id, 10)
       id++
-      db.put('_id', parseInt(id))
+      db.put('_id', id)
       return id
-   }).then(() => res.end())
-  
-   
+   }).then(res.end())
 }
 
 module.exports = {
--- db.js
+++ db.js
@@ -1,6 +1,7 @@
+const path = require('path')
 const level = require('level')
 
-const path = require('path')
+
 
 const dbPath = process.env.DB_PATH || path.join(__dirname, 'mydb')
 
